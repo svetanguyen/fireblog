505fba4c6502012376f5fc9cc0da0f63c17f2c33
svetanguyen <matematicaforeber@gmail.com>
2021-06-05 23:06:11 +0700

    create navigation

---
+++ src/components/Navigation.vue
@@ -0,0 +1,32 @@
+<template>
+  <header>
+      <nav class="container">
+          <div class="branding">
+              <router-link class="header" :to="{name: Home}">FireBlogs</router-link>
+          </div>
+          <div class="nav-links">
+              <ul>
+                  <router-links class="link" to="#">Home</router-links>
+                  <router-links class="link" to="#">Blogs</router-links>
+                  <router-links class="link" to="#">Create Post</router-links>
+                  <router-links class="link" to="#">Login/Register</router-links>
+              </ul>
+          </div>
+      </nav>
+      <menuIcon />
+  </header>
+</template>
+
+<script>
+import menuIcon from '../assets/Icons/bars-regular.svg';
+export default {
+    name: 'navigation',
+    components: {
+        menuIcon
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
