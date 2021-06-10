034cb98f55d3e552a04ad294c64d3caa86f722ea
svetanguyen <matematicaforeber@gmail.com>
2021-06-10 23:31:00 +0700

    import components

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -36,8 +36,18 @@
 </template>
 
 <script>
+import youTube from "../assets/Icons/youtube-brands.svg";
+import twitter from "../assets/Icons/twitter-brands.svg";
+import instagram from "../assets/Icons/instagram-brands.svg";
+import linkedin from "../assets/Icons/linkedin-brands.svg";
 export default {
-
+    name: "footer-vue",
+    components: {
+        youTube,
+        twitter,
+        instagram,
+        linkedin,
+    },
 }
 </script>
 
