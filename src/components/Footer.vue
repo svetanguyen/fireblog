ca826e89333c4bc3ddb0836d8d85a65fb6171e90
svetanguyen <matematicaforeber@gmail.com>
2021-06-10 23:29:41 +0700

    create footer, layout

---
+++ src/components/Footer.vue
@@ -0,0 +1,46 @@
+<template>
+  <footer>
+    <div class="container">
+      <div class="left">
+        <div class="col-1">
+          <router-link class="header" :to="{ name: 'Home' }">FireBlogs</router-link>
+          <ul>
+            <li>
+              <a href="#"><youTube class="svg-icon"/></a>
+            </li>
+            <li>
+              <a href="#"><twitter class="svg-icon"/></a>
+            </li>
+            <li>
+              <a href="#"><instagram class="svg-icon"/></a>
+            </li>
+            <li>
+              <a href="#"><linkedin class="svg-icon"/></a>
+            </li>
+          </ul>
+        </div>
+        <div class="col-2">
+          <ul>
+            <router-link class="link" :to="{ name: 'Home' }">Home</router-link>
+            <router-link class="link" :to="{ name: 'Blogs' }">Blogs</router-link>
+            <router-link v-if="admin" class="link" :to="{ name: 'CreatePost' }">Create Post</router-link>
+            <router-link v-if="!user" class="link" :to="{ name: 'Login' }">Login In / Register</router-link>
+          </ul>
+        </div>
+      </div>
+      <div class="right">
+        <p>Copyright 2021 All Rights Reserved</p>
+      </div>
+    </div>
+  </footer>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
