6cfd4b746a4fb5a7115541b6118a200a72408f76
svetanguyen <matematicaforeber@gmail.com>
2021-06-20 21:46:53 +0700

    add the blog cards

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,14 +2,23 @@
   <div class="home">
     <BlogPost :post="welcomeScreen" />
     <BlogPost :post="post" v-for="(post, index) in sampleBlogPost" :key="index" />
+    <div class="blog-card-wrap">
+      <div class="container">
+        <h3>View more recent blogs</h3>
+        <div class="blog-cards">
+          <BlogCards v-for="(post, index) in sampleBlogCards" :key="index" />
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 import BlogPost from '../components/BlogPost.vue';
+import BlogCards from '../components/BlogCard.vue';
 export default {
   name: "Home",
-  components: {BlogPost},
+  components: {BlogPost,BlogCards},
   data() {
     return {
       welcomeScreen: {
