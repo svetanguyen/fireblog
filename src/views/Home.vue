ecf1238b80d9ba25955aeecc91dee628a4e2c4df
svetanguyen <matematicaforeber@gmail.com>
2021-06-21 22:37:56 +0700

    style blog cards

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -61,3 +61,13 @@ export default {
   }
 };
 </script>
+
+<style lang="scss" scoped>
+  .blog-card-wrap {
+    h3 {
+      font-weight: 300;
+      font-size: 28px;
+      margin-bottom: 32px;
+    }
+  }
+</style>
