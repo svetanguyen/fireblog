de8bfc27a7de725715b3308a384f1553c69ab5bf
svetanguyen <matematicaforeber@gmail.com>
2021-06-24 23:11:50 +0700

    blog card layout

--- src/components/BlogCard.vue
+++ src/components/BlogCard.vue
@@ -1,12 +1,36 @@
 <template>
-  <div>
-    <h2>Hello</h2>
+  <div class="blog-card">
+    <div class="icons">
+      <div class="icon">
+        <Edit class="icon" />
+      </div>
+      <div class="icon">
+        <Delete class="icon" />
+      </div>
+    </div>
+    <img :src="require(`../assets/blogCards/${post.blogCoverPhoto}.jpg`)" alt="">
+    <div class="info">
+      <h4>{{ post.blogTitle }}</h4>
+      <h6>Posted on: {{ post.blogDate }}</h6>
+      <router-link class="link" to="#">
+        View the post <Arrow class="arrow" />
+      </router-link>
+    </div>
   </div>
 </template>
 
 <script>
+import Arrow from '../assets/Icons/arrow-right-light.svg'
+import Edit from '../assets/Icons/edit-regular.svg'
+import Delete from '../assets/Icons/trash-regular.svg'
 export default {
-
+  name: "blogCard",
+  props: ["post"],
+  components: {
+    Arrow,
+    Edit,
+    Delete
+  }
 }
 </script>
 
