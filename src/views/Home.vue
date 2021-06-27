167d0712e93320fb0212694bdf9e4143dc5a7b62
svetanguyen <matematicaforeber@gmail.com>
2021-06-27 22:02:12 +0700

    register section style

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -90,6 +90,29 @@ export default {
         padding: 125px 25px;
         flex-direction: row;
       }
+
+      .router-button {
+        display: flex;
+        font-size: 14px;
+        text-decoration: none;
+        @media(min-width: 800px) {
+          margin-left: auto;
+        }
+      }
+
+      h2 {
+        font-weight: 300;
+        font-size: 32px;
+        max-width: 425px;
+        width: 100%;
+        text-align: center;
+        text-transform: uppercase;
+        @media(min-width: 800px) {
+          text-align: initial;
+          font-size: 40px;
+        }
+      }
     }
+
   }
 </style>
